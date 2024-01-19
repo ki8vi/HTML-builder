35923f09e1536533f4a6e034685e1e2c246a643b
ki8vi <oneofbln@gmail.com>
2024-01-19 13:53:46 +0600

    feat: 05-merge-styles task is complete

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const process = require('node:process');
+const stylesDataArr = new Array();
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+  .then((files) => {
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].isFile() && path.extname(files[i].name) === '.css') {
+        fs.readFile(path.join(__dirname, 'styles', files[i].name))
+          .then((ch) => {
+            stylesDataArr.push(ch);
+            fs.writeFile(
+              path.join(__dirname, 'project-dist', 'bundle.css'),
+              stylesDataArr,
+            );
+          })
+          .catch((err) => process.stdout.write(err.message));
+      }
+    }
+  })
+  .catch((err) => process.stdout.write(err.message));
