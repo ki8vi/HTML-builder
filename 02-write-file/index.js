35009708875e366840b24c6efd664b08fcbf2c69
ki8vi <oneofbln@gmail.com>
2024-01-20 18:04:04 +0600

    fix: remove flag in 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,14 +3,9 @@ const path = require('node:path');
 const fs = require('node:fs');
 const readline = require('node:readline');
 const exit = 'Or type "exit" for quit!\n';
-const flag = {
-  flags: 'a',
-};
+
 const rl = readline.createInterface(process.stdin, process.stdout);
-const streamWrite = fs.createWriteStream(
-  path.join(__dirname, 'file.txt'),
-  flag,
-);
+const streamWrite = fs.createWriteStream(path.join(__dirname, 'file.txt'));
 
 process.stdout.write(`Lets type something here my friend! ${exit}`);
 rl.on('line', (content) =>
@@ -25,5 +20,4 @@ process.on('exit', () => {
 
 function enterToConsole(content) {
   streamWrite.write(content + '\n');
-  process.stdout.write(`Text added! Something else? ${exit}`);
 }
